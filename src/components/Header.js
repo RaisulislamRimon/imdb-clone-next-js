59d1a28f79000cfaf4a1e27411ec865c00c1e1ed
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-04-10 10:26:13 +0000

    update navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsInfoCircleFill } from 'react-icons/bs'
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <div>
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center my-6">
       <div className="flex">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="About" address="/about" Icon={BsInfoCircleFill} />
-
       </div>
       <div>
-
-      </div>
-      <div>
-
+        <Link href="/">
+          <h2 className="text-2xl">
+            <p className="font-bold bg-amber-500 py-1 px-2  rounded-lg inline mx-1 ">IMDB</p>
+            <p className="text-xl hidden sm:inline ">Clone</p>
+          </h2>
+        </Link>
       </div>
     </div>
   );

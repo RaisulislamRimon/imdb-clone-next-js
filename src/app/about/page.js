69ac2c7da38b0921ea2ca5d3c75e1e0587cd5fed
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-05-05 11:39:27 +0600

    added about page

---
+++ src/app/about/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt,
+        tempora inventore nisi earum, necessitatibus in atque, perspiciatis
+        veritatis beatae nam sint dolore. Placeat dignissimos quaerat in quo
+        itaque quisquam dolorem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+        architecto expedita debitis maxime id magni voluptate fugit porro omnis
+        deleniti, cumque ab? Sapiente iusto accusantium expedita ratione nam,
+        sed laudantium?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, ipsa
+        consequuntur, sint, alias perferendis doloremque culpa ex provident
+        saepe omnis corrupti molestiae eos libero quo. Vel iste repellat sunt
+        consectetur!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est natus rerum
+        commodi minus? Rem labore eaque debitis laborum ducimus facilis aliquid
+        omnis cumque delectus. In provident corrupti voluptas rem eum.
+      </p>
+    </div>
+  );
+};
+
+export default About;

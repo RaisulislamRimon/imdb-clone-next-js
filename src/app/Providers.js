4ca90850a69975027752a6b07a8372540854c292
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-05-05 11:50:24 +0600

    update

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -7,10 +7,10 @@ export default function Providers({ children }) {
     <ThemeProvider
       enableSystem={true}
       attribute="class"
-      className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen"
+      // className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen"
     >
-      <div>
-        {/* <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen"> */}
+      {/* <div> */}
+      <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen">
         {children}
       </div>
     </ThemeProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center my-6">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="About" address="/about" Icon={BsInfoCircleFill} />
       </div>
       <div className="flex items-center space-x-5">
-        <DarkModeSwitch />  
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl">
             <p className="font-bold bg-amber-500 py-1 px-2  rounded-lg inline mx-1 ">

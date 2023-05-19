744e9232bdd982048ae402e451797af2f8f7c802
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-05-19 10:42:01 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,37 @@ const inter = Inter({ subsets: ["latin"] });
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
-export default function Home() {
+// async function getData({ searchParams }) {
+// const genre = searchParams.genre || "fetchTrending";
+// const res = await fetch(
+//   `https://api.themoviedb.org/3/
+// ${
+//   genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+// }?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+//   { next: { revalidate: 10 } }
+// );
+//   return res.json();
+// }
+
+export default async function Home({ searchParams }) {
+  // const data = getData();
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/
+  ${
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10 } }
+  );
+  // return res.json();
+  const data = res.json();
+  // return data;
+  // const
+
   return (
     <main>
       <h1>Home</h1>
+      {data?.length}
     </main>
   );
 }

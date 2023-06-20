b8ed9f4202ecdd8e19836fb6c3b56a20b554cbfc
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-06-20 16:08:01 +0000

    showing movie poster in movie card

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  // images
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Card = ({ result }) => {
   return (
-    <div>
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
       <div>
-        {/* {result.id} */}
-        <p>{result.title}</p>
-        <h1>Title</h1>
-        <p>Overview</p>
+        <Link href={`/movie/${result.id}`}>
+          {/* {result.id} */}
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${
+              result.backdrop_path || result.poster_path
+            }`}
+            width={500}
+            height={300}
+            alt={result.title || result.original_name}
+          />
+          <p>{result.title}</p>
+          <h1>Title</h1>
+          <p>Overview</p>
+        </Link>
       </div>
     </div>
   );

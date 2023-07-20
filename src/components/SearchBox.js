cea6f39c3e08f4644d80d41debcca5e2e9015533
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-07-20 13:31:17 +0000

    added searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,7 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function SearchBox() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(search);
+    if (!search) return;
+    router.push(`/search/${search}`);
+  };
   return (
-    <div>
-      <h1>Search Box</h1>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex max-w-6xl mx-auto justify-between items-center px-5"
+    >
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Search keywords..."
+        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" className="text-amber-600 disabled:text-gray-400">
+        Search
+      </button>
+    </form>
   );
 }

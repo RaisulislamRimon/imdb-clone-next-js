fc5f9bc8182635bdbf4d603f8ad98b07a2cb2a0a
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-07-20 10:55:24 +0000

    create movie page

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,9 +1,18 @@
-export default function Movie({ params }) {
+async function getMovie(movieId) {
+  // console.log(movieId);
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`
+  );
+  return await res.json();
+}
+
+export default async function Movie({ params }) {
   const movieId = params?.id;
-  console.log(movieId);
+  const movie = await getMovie(movieId);
+  console.log(movie);
   return (
     <div>
-      <h1>Movie page title</h1>
+      <h1>{movie?.title}</h1>
     </div>
   );
 }

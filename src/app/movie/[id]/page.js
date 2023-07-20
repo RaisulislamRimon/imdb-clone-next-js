aae260bb829f232254b2fec49f6e71493043fcba
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-07-20 11:09:39 +0000

    update the movie page

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function getMovie(movieId) {
   // console.log(movieId);
   const res = await fetch(
@@ -9,10 +11,27 @@ async function getMovie(movieId) {
 export default async function Movie({ params }) {
   const movieId = params?.id;
   const movie = await getMovie(movieId);
-  console.log(movie);
+  // console.log(movie);
   return (
-    <div>
-      <h1>{movie?.title}</h1>
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          width={500}
+          height={300}
+          // alt={result.title || result.original_name}
+          className="rounded-lg"
+          style={{
+            maxWidth: "100%",
+            height: "100%",
+          }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="Movie Poster Image"
+        />
+      </div>
     </div>
   );
 }
